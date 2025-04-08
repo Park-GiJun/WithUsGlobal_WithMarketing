1822b94709b36dd88bda173115c965358a68e634
tpgj9 <tpgj98@naver.com>
2025-04-08 21:44:45 +0900

    Initial commit

---
+++ src/main/kotlin/com/gijun/withusglobal/WithUsGlobalApplication.kt
@@ -0,0 +1,11 @@
+package com.gijun.withusglobal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WithUsGlobalApplication
+
+fun main(args: Array<String>) {
+    runApplication<WithUsGlobalApplication>(*args)
+}
---
+++ src/test/kotlin/com/gijun/withusglobal/WithUsGlobalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gijun.withusglobal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WithUsGlobalApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
